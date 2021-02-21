3b1e9a448fed3404ff9dbdacd39d40eaeebdcfe4
Eli W. Hunter <elihunter173@gmail.com>
2021-02-21 14:37:30 -0500

    Hook up new options and reworked runner to discord
    Also adds new #!run command for alternative syntax for options

--- src/discord.rs
+++ src/discord.rs
@@ -9,8 +9,15 @@ use serenity::{
     prelude::{Context, EventHandler},
     utils::Color,
 };
+use tokio::sync::mpsc::{self, Sender};
 
-use crate::{db::MessageIds, runner::CodeRunner};
+use crate::{
+    db::MessageIds,
+    options_parser::parse_options,
+    runner::{CodeRunner, UnrecognizedContainer},
+};
+
+// TODO: Do I want to react to message when I send them?
 
 pub struct Handler {
     pub language_text: Box<str>,
@@ -18,55 +25,117 @@ pub struct Handler {
     pub message_ids: MessageIds,
 }
 
-// TODO: When there is no code given, Discord says that the language code is the code while we say
-// the language code is the language code. We should agree with discord always
-static CODE_BLOCK: Lazy<Regex> =
-    Lazy::new(|| Regex::new(r"(?sm)```(?P<lang>\S*)\n(?P<code>.*)```").unwrap());
+async fn should_run(ctx: &Context, msg: &Message) -> bool {
+    msg.content.starts_with("#!run") || msg.mentions_me(ctx).await.unwrap() || msg.is_private()
+}
 
-impl Handler {
-    async fn try_run_raw(&self, msg: &str) -> String {
-        log::debug!("Responding to {:#?}", msg);
-        let (lang, code) = match CODE_BLOCK.captures(msg) {
+#[derive(Debug, Eq, PartialEq)]
+struct RunMessage<'a> {
+    opts: &'a str,
+    lang: &'a str,
+    code: &'a str,
+}
+
+fn parse_message(msg: &str) -> Option<RunMessage> {
+    // (?s) enables the 's' flag which lets . match '\n'
+    static MENTION_CODE_LANG: Lazy<Regex> =
+        Lazy::new(|| Regex::new(r"(?s)```(?P<lang>\S*)\n(?P<code>.*)```").unwrap());
+    static MENTION_OPTS: Lazy<Regex> = Lazy::new(|| Regex::new(r"`\[\[(?P<opts>.*)\]\]`").unwrap());
+
+    static CMD_RUN_ALL: Lazy<Regex> = Lazy::new(|| {
+        Regex::new(r"(?s)^#!run\s+(?P<opts>.*)\s+```(?P<lang>\S*)\n(?P<code>.*)```").unwrap()
+    });
+
+    if msg.starts_with("#!run") {
+        match CMD_RUN_ALL.captures(msg) {
+            Some(caps) => Some(RunMessage {
+                opts: caps.name("opts").unwrap().as_str(),
+                lang: caps.name("lang").unwrap().as_str(),
+                code: caps.name("code").unwrap().as_str(),
+            }),
+            None => None,
+        }
+    } else {
+        let (lang, code) = match MENTION_CODE_LANG.captures(msg) {
             Some(caps) => (
                 caps.name("lang").unwrap().as_str(),
                 caps.name("code").unwrap().as_str(),
             ),
-            None => {
-                let message = r"Were you trying to run some code? I couldn't find any code blocks in your message.
+            None => return None,
+        };
+        let opts = match MENTION_OPTS.captures(msg) {
+            Some(caps) => caps.name("opts").unwrap().as_str(),
+            None => "",
+        };
+        Some(RunMessage { opts, lang, code })
+    }
+}
+
+// XXX: Ideally this would use generators rather than a channel...
+async fn try_run_raw(runner: &CodeRunner, msg: &str, tx: Sender<String>) {
+    macro_rules! send {
+        ($($arg:tt)*) => ( tx.send(format!($($arg)*)).await.unwrap() )
+    }
+    macro_rules! bail {
+        ($($arg:tt)*) => ( return send!($($arg)*) )
+    }
+
+    log::debug!("Responding to {:#?}", msg);
+    let run = match parse_message(msg) {
+        Some(run) => run,
+        None => bail!(
+            r"Were you trying to run some code? I couldn't find any code blocks in your message.
 
 Be sure to annotate your code blocks with a language like
 \`\`\`python
 print('Hello World')
-\`\`\`";
-                return message.into();
-            }
-        };
-        if lang.is_empty() {
-            return format!(
-                r"I noticed you sent a code block but didn't include a language tag, so I don't know how to run it. The language goes immediately after the \`\`\` like so
+\`\`\`"
+        ),
+    };
+    if run.lang.is_empty() {
+        bail!(
+            r"I noticed you sent a code block but didn't include a language tag, so I don't know how to run it. The language goes immediately after the \`\`\` like so
 
 \`\`\`your-language-here
 {code}\`\`\`",
-                code = code
-            );
-        }
+            code = run.code
+        );
+    }
+    let opts = match parse_options(run.opts) {
+        Ok(opts) => opts,
+        // TODO: Improve error messages
+        Err(err) => bail!("{}", err),
+    };
 
-        log::debug!("language: {:?}, code: {:?}", lang, code);
-        let lang = match self.bot.get_lang_by_code(lang) {
-            Some(lang) => lang,
-            None => {
-                // TODO: Get suggestions using strsim
-                return format!(
-                    "I'm sorry. I don't know how to run `{}` code snippets.",
-                    lang
-                );
+    log::debug!("{:?}", run);
+    let lang_ref = match runner.get_lang_by_code(run.lang) {
+        Some(lang) => lang,
+        // TODO: Get suggestions using strsim
+        None => bail!(
+            "I'm sorry. I don't know how to run `{}` code snippets.",
+            run.lang,
+        ),
+    };
+
+    let run_spec = match lang_ref.run_spec(opts) {
+        Ok(run_spec) => run_spec,
+        Err(err) => bail!("{}", err),
+    };
+    match runner.run_code(&run_spec, run.code).await {
+        Ok(output) => send!("{}", output),
+        Err(err) => match err.downcast_ref::<UnrecognizedContainer>() {
+            Some(_) => {
+                send!("Building container. Please be patient. This may take awhile.");
+                if let Err(err) = runner.build(&run_spec).await {
+                    bail!("{}", err);
+                }
+                match runner.run_code(&run_spec, run.code).await {
+                    Ok(output) => send!("{}", output),
+                    Err(err) => bail!("{}", err),
+                }
             }
-        };
-
-        match self.bot.run_code(lang, code).await {
-            Ok(output) => format!("{}", output),
-            Err(err) => format!("{}", err),
-        }
+            None => bail!("{}", err),
+        },
     }
 }
 
@@ -83,61 +152,56 @@ impl EventHandler for Handler {
         &self,
         ctx: Context,
         _old_if_available: Option<Message>,
-        _new: Option<Message>,
-        event: MessageUpdateEvent,
+        new: Option<Message>,
+        _event: MessageUpdateEvent,
     ) {
-        // TODO: Remove all reactions upon update?
+        // TODO: (This is a discord bug.) For some reason she seems to ping the user when the
+        // message is updated but not when it is initially sent. Preferably she would never ping
+        // the user because that can be annoying.
 
-        // Says message is private or metions us
-        let my_id = ctx.cache.current_user_id().await;
-        let mentions_me = if let Some(mentions) = &event.mentions {
-            mentions.iter().any(|user| user.id == my_id)
-        } else {
-            false
+        let msg = match new {
+            Some(msg) => msg,
+            None => return,
         };
 
-        // Don't respond if it's a group message that doesn't mention us
-        if event.guild_id.is_some() && !mentions_me {
+        if !should_run(&ctx, &msg).await {
             return;
         }
 
-        let reply_id = if let Some(reply_id) = self.message_ids.get(event.id).unwrap() {
-            reply_id
-        } else {
-            let msg = event
-                .channel_id
-                .message(&ctx, event.id)
-                .await
-                .expect("failed to get handle on message");
-            msg.react(&ctx, '❌')
-                .await
-                .expect("failed to reach to message");
-            return;
-        };
-
-        event
-            .channel_id
-            .edit_message(&ctx, reply_id, |builder| builder.content("Re-running code"))
-            .await
-            .expect("failed to edit message");
-        let body = self
-            .try_run_raw(&event.content.as_ref().expect("failed to find message body"))
-            .await;
-
-        match event
-            .channel_id
-            .edit_message(&ctx, reply_id, |builder| builder.content(body))
-            .await
-        {
-            Ok(_) => {}
-            Err(err) => {
-                event
-                    .channel_id
-                    .edit_message(&ctx, reply_id, |builder| builder.content(err))
+        let reply_id = match self.message_ids.get(msg.id).unwrap() {
+            Some(reply_id) => reply_id,
+            None => {
+                msg.react(&ctx, '❌')
                     .await
-                    .expect("failed to edit message");
+                    .expect("failed to react to message");
+                return;
             }
-        }
+        };
+
+        let runner = &self.bot;
+        let (tx, mut rx) = mpsc::channel(2);
+        tokio::join!(
+            async {
+                try_run_raw(runner, &msg.content, tx).await;
+            },
+            async {
+                while let Some(ref body) = rx.recv().await {
+                    match msg
+                        .channel_id
+                        .edit_message(&ctx, reply_id, |builder| builder.content(body))
+                        .await
+                    {
+                        Ok(_) => {}
+                        Err(err) => {
+                            msg.channel_id
+                                .edit_message(&ctx, reply_id, |builder| builder.content(err))
+                                .await
+                                .expect("failed to edit message");
+                        }
+                    }
+                }
+            }
+        );
     }
 
     async fn message(&self, ctx: Context, msg: Message) {
@@ -156,8 +220,6 @@ print("Hello, World!")
             msg.channel_id
                 .send_message(&ctx, |m| {
                     m.embed(|e| {
-                        // TODO: Should I use .author instead of .title? It's smaller but it can
-                        // include an icon and isn't blue
                         e.title("Codie the Code Runner")
                             .url("https://github.com/elihunter173/codie-discord")
                             .footer(|f| {
@@ -171,18 +233,35 @@ print("Hello, World!")
                 })
                 .await
                 .expect("failed to send help message");
-        } else if msg.is_private() || msg.mentions_me(&ctx).await.unwrap() {
-            msg.react(&ctx, '🤖')
-                .await
-                .expect("failed to react to message");
-            let body = self.try_run_raw(&msg.content).await;
-            let reply = msg
-                .reply(&ctx, body)
-                .await
-                .expect("failed to reply to message");
-            if let Some(_) = self.message_ids.insert(msg.id, reply.id).unwrap() {
-                panic!("colliding message ids");
-            }
+        } else if should_run(&ctx, &msg).await {
+            let runner = &self.bot;
+            let (tx, mut rx) = mpsc::channel(2);
+            tokio::join!(
+                async {
+                    try_run_raw(runner, &msg.content, tx).await;
+                },
+                async {
+                    let body = rx.recv().await.expect("at least one message");
+                    let mut reply = msg
+                        .reply(&ctx, body)
+                        .await
+                        .expect("failed to reply to message");
+                    while let Some(ref body) = rx.recv().await {
+                        match reply.edit(&ctx, |builder| builder.content(body)).await {
+                            Ok(_) => {}
+                            Err(err) => {
+                                reply
+                                    .edit(&ctx, |builder| builder.content(err))
+                                    .await
+                                    .expect("failed to edit message");
+                            }
+                        }
+                    }
+                    if let Some(_) = self.message_ids.insert(msg.id, reply.id).unwrap() {
+                        panic!("colliding message ids");
+                    }
+                }
+            );
         } else if msg.content.starts_with("#!") {
             msg.reply(&ctx, "I'm sorry. I didn't recognize that command")
                 .await
@@ -192,5 +271,47 @@ print("Hello, World!")
 
     async fn ready(&self, ctx: Context, _data_about_bot: Ready) {
         ctx.set_activity(Activity::listening("#!help")).await;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_message_empty() {
+        assert_eq!(parse_message(""), None);
+    }
+
+    #[test]
+    fn test_parse_message_random() {
+        assert_eq!(
+            parse_message("Hey! I just wanted to check up on your progress on the project. Do you think you could have your part done by tomorrow?"),
+            None,
+        );
+    }
+
+    #[test]
+    fn test_parse_message_run_cmd() {
+        assert_eq!(
+            parse_message("#!run version=3.8 ```py\nprint('Hello, World!')\n```"),
+            Some(RunMessage {
+                lang: "py",
+                opts: "version=3.8",
+                code: "print('Hello, World!')\n",
+            }),
+        );
+    }
+
+    #[test]
+    fn test_parse_message_mention() {
+        assert_eq!(
+            parse_message("@Codie `[[version=3.8]]` ```py\nprint('Hello, World!')\n```"),
+            Some(RunMessage {
+                lang: "py",
+                opts: "version=3.8",
+                code: "print('Hello, World!')\n",
+            }),
+        );
     }
 }
